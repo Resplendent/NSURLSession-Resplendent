fbba9c588f15ac4e28db8aeb307aedfc63df9ef5
Lee Pollard <tjdet11@gmail.com>
2015-05-03 13:55:26 -0400

    Initial commit

---
+++ Example/NSURLSession-Resplendent/RUViewController.h
@@ -0,0 +1,13 @@
+//
+//  RUViewController.h
+//  NSURLSession-Resplendent
+//
+//  Created by Lee Pollard on 05/03/2015.
+//  Copyright (c) 2014 Lee Pollard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUViewController : UIViewController
+
+@end
---
+++ Example/NSURLSession-Resplendent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSURLSession-Resplendent
+//
+//  Created by Lee Pollard on 05/03/2015.
+//  Copyright (c) 2014 Lee Pollard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RUAppDelegate class]));
+    }
+}
